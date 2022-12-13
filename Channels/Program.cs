1d73c82425710220bdb6ff0452784ddeda3d9b1d
aqz-hub <fanatmiracle@yandex.ru>
2022-12-13 12:12:44 +0500

    reworked on Threading.Channels

--- Channels/Program.cs
+++ Channels/Program.cs
@@ -1,100 +1,26 @@
 ﻿using System.Diagnostics;
 using System.Net.NetworkInformation;
+using System.Reflection.PortableExecutable;
+using System.Threading.Channels;
 
 namespace Channels
 {
     internal class Program
     {
-        private static List<int> numbers = new List<int>();
-        private static List<int> tens = new List<int>();
-        private static int _lastNumber;
-        public static int LastNumber
-        {
-            get { return _lastNumber; }
-            set
-            {
-                if (_lastNumber != value)
-                {
-                    _lastNumber = value;
-                    numbers.Add(value);
-                }
-            }
-        }
-        private static int _lastTen;
-        public static int LastTen
-        {
-            get { return _lastTen; }
-            set
-            {
-                if(_lastTen != value)
-                {
-                    _lastTen = value;
-                    tens.Add(value);
-                }
-            }
-        }
+        public static Channel<int> TenChannel = Channel.CreateUnbounded<int>();
+        public static ChannelWriter<int> TenWriter = TenChannel.Writer;
+        public static ChannelReader<int> TenReader = TenChannel.Reader;
+
+        public static Channel<int> NumberChannel = Channel.CreateUnbounded<int>();
+        public static ChannelWriter<int> NumberWriter = NumberChannel.Writer;
+        public static ChannelReader<int> NumberReader = NumberChannel.Reader;
         static async Task Main(string[] args)
         {
-            var task = WriteAsync();
-            Read();
+            var task = Run();
+
             task.Wait();
         }
-        public static void Read()
-        {
-            int number = 0;
-            int ten = 0;
-            while (true)
-            {
-                if(number == 0 && ten == 0)
-                {
-                    number = LastNumber;
-                    ten = LastTen;
-                    Console.WriteLine(number.ToString() + " " + ten.ToString());
-                }
-                if (number != LastNumber && ten != LastTen)
-                {
-                    Console.WriteLine(LastNumber.ToString() + " " + LastTen.ToString());
-                    number = LastNumber;
-                    ten = LastTen;
-                }
-            }
-        }
-        public static void WriteNumber(int value)
-        {
-            LastNumber = value;
-        }
-        public static void WriteTen(int value)
-        {
-            LastTen = value;
-        }
-        public static int GetDelay()
-        {
-            return new Random().Next(500, 1000);
-        }
-        public static async Task WriteNumbersAsync()
-        {
-            int number = 0;
-            while(number < 10)
-            {
-                number++;
-                WriteNumber(number);
-                await Task.Delay(GetDelay());
-                await Task.Yield();
-            }
-        }
-        public static async Task WriteTensAsync()
-        {
-            int number = 0;
-            while(number < 100)
-            {
-                number += 10;
-                WriteTen(number);
-                await Task.Delay(GetDelay());
-                await Task.Yield();
-            }
-        }
-
-        static async Task WriteAsync()
+        static async Task Run()
         {
             var taskNumber = WriteNumbersAsync();
             var taskTens = WriteTensAsync();
@@ -104,6 +30,40 @@ namespace Channels
                 var ready = await Task.WhenAny(tasks);
                 tasks.Remove(ready);
             }
+        }
+        public static int GetDelay()
+        {
+            return new Random().Next(500, 1000);
+        }
+        public static async Task WriteAsync(ChannelWriter<int> writer, int value)
+        {
+            await writer.WriteAsync(value);
+        }
+        public static async Task WriteNumbersAsync()
+        {
+            int number = 0;
+            while (number < 10)
+            {
+                number++;
+                await WriteAsync(NumberWriter, number);
+                await Task.Delay(GetDelay());
+                int valueFromChannel = await NumberReader.ReadAsync();
+                Console.WriteLine(valueFromChannel);
+            }
+            NumberWriter.Complete();
+        }
+        public static async Task WriteTensAsync()
+        {
+            int ten = 0;
+            while(ten < 100)
+            {
+                ten += 10;
+                await WriteAsync(TenWriter, ten);
+                await Task.Delay(GetDelay());
+                int valueFromChannel = await TenReader.ReadAsync();
+                Console.WriteLine(valueFromChannel);
+            }
+            TenWriter.Complete();
         }
     }
 }
